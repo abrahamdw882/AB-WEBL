feeb28f1fda028783525dfc418fc77e8be41b3a6
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2025-04-10 22:22:55 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 const header = document.querySelector("header");
 
 window.addEventListener("scroll", function() {
-    header.classList.toggle("sticky",this.window.scrollY > 120);
-})
+    header.classList.toggle("sticky", window.scrollY > 120);
+});
+
 let menu = document.querySelector("#menu-icon");
 let navlist = document.querySelector('.navlist');
 
-menu.onclick = () =>{
+menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navlist.classList.toggle('active');
-}
+};
 
-window.onscroll = () =>{
-    menu.remove.toggle('bx-x');
-    navlist.remove.toggle('active');
-}
+window.onscroll = () => {
+    if (window.scrollY > 120) {
+        menu.classList.remove('bx-x');
+        navlist.classList.remove('active');
+    }
+};
